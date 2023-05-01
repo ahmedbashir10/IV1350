43cd6b805af3d17bf552f96913dfc1d608814365
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-01 21:47:14 +0200

    added Identifier class in the model

---
+++ src/se/kth/iv1350/POS/model/Identifier.java
@@ -0,0 +1,15 @@
+package se.kth.iv1350.POS.model;
+
+import controller.Controller;
+
+private class Identifier {
+
+	private String barcode;
+
+	private Controller controller;
+
+	public Identifier identifier(String barcode) {
+		return null;
+	}
+
+}
