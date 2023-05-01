b89f9e1d97a54d83fe236fe31f8b6e69b0b24b1f
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-01 21:48:07 +0200

    added Receipt class in the model

---
+++ src/se/kth/iv1350/POS/model/Receipt.java
@@ -0,0 +1,45 @@
+package se.kth.iv1350.POS.model;
+
+import integration.itemDTO;
+
+public class Receipt {
+
+	private int totalprice;
+
+	private int change;
+
+	private int date;
+
+	private int time;
+
+	private double tax;
+
+	private itemDTO itemDTO;
+
+	private Sale sale;
+
+	Receipt Receipt() {
+		return null;
+	}
+
+	void addInfoOfThisItem(itemDTO itemDTO) {
+
+	}
+
+	void addInfoWithQuantity(itemDTO itemDTO, int quantity) {
+
+	}
+
+	void addInfo(Sale sale) {
+
+	}
+
+	void addPrice(double totalPrice) {
+
+	}
+
+	void addPriceAfterDiscount(double totalPriceAfterDiscount) {
+
+	}
+
+}
