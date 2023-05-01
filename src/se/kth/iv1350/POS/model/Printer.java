36508181ed9abe932f84ef1599194e4484310a34
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-01 21:47:40 +0200

    added Printer class in the model

---
+++ src/se/kth/iv1350/POS/model/Printer.java
@@ -0,0 +1,19 @@
+package se.kth.iv1350.POS.model;
+
+import startup.main;
+
+class Printer {
+
+	private Sale sale;
+
+	private main main;
+
+	public Printer printer() {
+		return null;
+	}
+
+	void printReceipt(Receipt receipt) {
+
+	}
+
+}
