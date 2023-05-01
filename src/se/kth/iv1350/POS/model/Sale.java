6fa40c15a02f03efc73fa20fac3612d1dabd2ec0
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-01 21:48:30 +0200

    added Sale class in the model

---
+++ src/se/kth/iv1350/POS/model/Sale.java
@@ -0,0 +1,61 @@
+package se.kth.iv1350.POS.model;
+
+import controller.Controller;
+import integration.itemDTO;
+import integration.Discount;
+
+public class Sale {
+
+	private java.lang.LocalTime saleTime;
+
+	private double totalPrice;
+
+	private double change;
+
+	private Receipt receipt;
+
+	private Controller controller;
+
+	private Printer printer;
+
+	public Sale Sale() {
+		return null;
+	}
+
+	private void setTimeOfSale() {
+
+	}
+
+	public void addItem(itemDTO itemDTO) {
+
+	}
+
+	public void addItemQuantity(int itemDTO, int quantity) {
+
+	}
+
+	public double getPrice() {
+		return 0;
+	}
+
+	public void addTotalPrice(int totalPrice) {
+
+	}
+
+	public Sale getThisSale() {
+		return null;
+	}
+
+	public double getChange(double totalAmount) {
+		return 0;
+	}
+
+	public void endSale() {
+
+	}
+
+	public double getPriceAfterDiscount(Discount discount) {
+		return 0;
+	}
+
+}
