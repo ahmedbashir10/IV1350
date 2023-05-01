592142bc87604ffeeac52da2718650bf8f388173
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-01 21:43:20 +0200

    added controller class

---
+++ src/se/kth/iv1350/POS/controller/Controller.java
@@ -0,0 +1,59 @@
+package controller;
+
+import view.View;
+import model.Sale;
+import startup.main;
+import integration.InventorySystem;
+import integration.AccountingSystem;
+import integration.DiscountDataBase;
+import model.Customer;
+import model.Identifier;
+import integration.itemDTO;
+
+public class Controller {
+
+	private View view;
+
+	private Sale sale;
+
+	private main main;
+
+	private InventorySystem inventorySystem;
+
+	private AccountingSystem accountingSystem;
+
+	private DiscountDataBase discountDataBase;
+
+	private Customer customer;
+
+	private Identifier identifier;
+
+	public void startSale() {
+
+	}
+
+	public Controller Controller() {
+		return null;
+	}
+
+	public void increaseItemQuantity(itemDTO itemDTO, int quantity) {
+
+	}
+
+	public double endSale() {
+		return 0;
+	}
+
+	public double getPriceAfterDiscount() {
+		return 0;
+	}
+
+	public double getChange(double totalAmount) {
+		return 0;
+	}
+
+	public itemDTO scanItem(java.lang.String barcode) {
+		return null;
+	}
+
+}
--- src/se/kth/iv1350/POS/startup/Main.java
+++ src/se/kth/iv1350/POS/startup/Main.java
@@ -1,3 +1,5 @@
+package se.kth.iv1350.POS.startup;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
