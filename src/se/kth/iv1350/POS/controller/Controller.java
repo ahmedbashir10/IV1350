61e82e63d50365406dd9c4d3dcc4c759d3aded49
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-03 23:23:14 +0200

    added some instans of other objects as well as implemented a constructor

--- src/se/kth/iv1350/POS/controller/Controller.java
+++ src/se/kth/iv1350/POS/controller/Controller.java
@@ -3,20 +3,12 @@ package se.kth.iv1350.POS.controller;
 import se.kth.iv1350.POS.integration.AccountingSystem;
 import se.kth.iv1350.POS.integration.DiscountDataBase;
 import se.kth.iv1350.POS.integration.InventorySystem;
-import se.kth.iv1350.POS.integration.itemDTO;
-import se.kth.iv1350.POS.model.Customer;
-import se.kth.iv1350.POS.model.Identifier;
+import se.kth.iv1350.POS.model.Printer;
 import se.kth.iv1350.POS.model.Sale;
-
-import javax.swing.text.View;
 
 public class Controller {
 
-	private View view;
-
 	private Sale sale;
-
-	//private main main;
 
 	private InventorySystem inventorySystem;
 
@@ -24,19 +16,17 @@ public class Controller {
 
 	private DiscountDataBase discountDataBase;
 
-	private Customer customer;
+	private Printer printer;
 
-	private Identifier identifier;
+
+	public Controller(InventorySystem inventorySystem, AccountingSystem accountingSystem, Printer printer) {
+		this.inventorySystem = inventorySystem;
+		this.accountingSystem = accountingSystem;
+		this.printer = printer;
+	}
 
 	public void startSale() {
-
-	}
-
-	public Controller Controller() {
-		return null;
-	}
-
-	public void increaseItemQuantity(itemDTO itemDTO, int quantity) {
+		sale = new Sale();
 
 	}
 
@@ -52,8 +42,5 @@ public class Controller {
 		return 0;
 	}
 
-	public itemDTO scanItem(java.lang.String barcode) {
-		return null;
-	}
 
 }
