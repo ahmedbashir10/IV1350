9472f464faee60567b5e6d48d74e423e2970ead9
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-04 00:28:03 +0200

    added getprice, gettaxrate and method that return a hashcode

--- src/se/kth/iv1350/POS/integration/ItemDTO.java
+++ src/se/kth/iv1350/POS/integration/ItemDTO.java
@@ -2,23 +2,41 @@ package se.kth.iv1350.POS.integration;
 
  public class ItemDTO {
 
-	private final String name;
+	private String description;
 
-	private final String description;
+	private  double price;
 
-	private final double price;
-
-	private final int taxRate;
-
-	private InventorySystem inventorySystem;
+	private int taxRate;
 
 
-	public ItemDTO(String name, String description, double price, int taxRate) {
-		this.name = name;
+	public ItemDTO(String description, double price, int taxRate) {
 		this.description = description;
 		this.price = price;
 		this.taxRate = taxRate;
 	}
 
 
-}
+	 public Object getPrice() {
+		return this.price;
+	 }
+
+	 public int getTaxRate() {
+		 return taxRate;
+	 }
+
+	 public String getDescription() {
+		 return description;
+	 }
+
+	 public int hashCode(){
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description === null) ? 0 : description.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^(temp >>> 32));
+		temp = Double.doubleToLongBits(taxRate);
+		result = prime *result + (int) (temp ^ (temp >>> 32));
+		return result;
+	 }
+ }
