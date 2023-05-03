e4b2ded521eeb069f3f6b92574fca50464ee4ce0
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-03 23:58:37 +0200

    implemented the receipt class which only have one construtor and one method

--- src/se/kth/iv1350/POS/model/Receipt.java
+++ src/se/kth/iv1350/POS/model/Receipt.java
@@ -1,45 +1,17 @@
 package se.kth.iv1350.POS.model;
 
-import se.kth.iv1350.POS.integration.itemDTO;
-
 public class Receipt {
-
-	private int totalprice;
-
-	private int change;
-
-	private int date;
-
-	private int time;
-
-	private double tax;
-
-	private itemDTO itemDTO;
-
 	private Sale sale;
 
 	Receipt Receipt() {
 		return null;
 	}
 
-	void addInfoOfThisItem(itemDTO itemDTO) {
 
+	void setSaleInformation(Sale sale) {
+		this.sale = sale;
 	}
-
-	void addInfoWithQuantity(itemDTO itemDTO, int quantity) {
-
-	}
-
-	void addInfo(Sale sale) {
-
-	}
-
-	void addPrice(double totalPrice) {
-
-	}
-
-	void addPriceAfterDiscount(double totalPriceAfterDiscount) {
-
-	}
-
+	//void addInfo(Sale sale) {
+	//}
 }
+
