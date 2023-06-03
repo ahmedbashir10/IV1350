03fac71385082208c82fd3a1d83e052e993dd2f7
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-06-04 01:31:40 +0200

    added receipt tostring method

--- src/se/kth/iv1350/POS/model/Receipt.java
+++ src/se/kth/iv1350/POS/model/Receipt.java
@@ -12,7 +12,7 @@ public class Receipt {
 	 *
 	 * @param sale The given sale.
 	 */
-	void setSaleInfo(Sale sale) {
+	public void setSaleInfo(Sale sale) {
 		this.sale = sale;
 	}
 
@@ -26,9 +26,53 @@ public class Receipt {
 		StringBuilder receiptBuilder = new StringBuilder();
 		receiptBuilder.append("RECEIPT");
 		receiptBuilder.append("\n");
-		receiptBuilder.append(sale.toString());
+		receiptBuilder.append(receipttoString());
 		return receiptBuilder.toString();
 
 	}
 
+	/**
+	 * Makes a string representation of this sale
+	 *
+	 * @return The string representation of this sale
+	 */
+
+	public String receipttoString() {
+		StringBuilder saleSB = new StringBuilder();
+
+
+		var itemSet = sale.getListOfItems().entrySet();
+		for (var entry : itemSet) {
+			saleSB.append("Item: ");
+			saleSB.append(entry.getKey().getItemDescription());
+			saleSB.append(" Quantity: ");
+			saleSB.append(entry.getValue());
+			appendNewLine(saleSB);
+		}
+
+		saleSB.append("Total price of the sale ");
+		saleSB.append(sale.getTotalPrice());
+
+		appendNewLine(saleSB);
+
+		saleSB.append("Total amount of VAT: ");
+		saleSB.append(sale.getTotalVAT());
+		appendNewLine(saleSB);
+
+		saleSB.append("Total amount paid: ");
+		saleSB.append(sale.getPaidAmount());
+		appendNewLine(saleSB);
+
+		saleSB.append("Total amount of change: ");
+		saleSB.append(sale.getChange());
+		appendNewLine(saleSB);
+
+		return saleSB.toString();
+
+	}
+
+	private void appendNewLine(StringBuilder StringBuilderToAppendNewLineOn) {
+		StringBuilderToAppendNewLineOn.append("\n");
+
+	}
 }
